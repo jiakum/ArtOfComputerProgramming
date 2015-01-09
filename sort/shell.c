0f10fb1d9660e64fbe2829e937d2ff13538ae760
Jiakun Ma <jkma@actions-semi.com>
2015-01-09 10:40:23 +0800

    update shell sort

--- sort/shell.c
+++ sort/shell.c
@@ -3,14 +3,27 @@
 #include <stdint.h>
 #include "data.c"
 
-static const int T = 1;
-static const int H[] = { 1, 3};
+static int T = 1;
+static const int H_LOWER[] = { 1, 3, 7, 16, 36, 81, 183, 412, 927, 2086};
+static const int H_HIGHER[] = { 1, 3, 7, 16, 36, 81, 183, 412, 927, 2086};
 
 int main(int argc,char *argv[])
 {
 	int h,t,i,j,n,k;
+	const int *H;
 
 	n = sizeof(data)/sizeof(int);
+	if(n < 1000)
+		H = H_LOWER;
+	else 
+		H = H_HIGHER;
+	for(i=0;;i++) {
+		if(H[i+2] > n) {
+			t = i; 
+			break;
+		}				
+	}
+
 	for(t=T; t>=0; t--) {
 		h = H[t];
 		for(i=h-1; i<n; i+=h) {
