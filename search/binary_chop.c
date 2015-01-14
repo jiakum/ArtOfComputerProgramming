e025773a568dca190db899b57994f3f2bcf8dc68
Jiakun Ma <jkma@actions-semi.com>
2015-01-14 11:10:24 +0800

    add binary chop function. data.c contains a increased sequential data

---
+++ search/binary_chop.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "data.c"
+
+int main()
+{
+	int n,l,r,success,i,k;
+
+	n = sizeof(data)/sizeof(data[0]);
+	l = 0; r = n-1; success = 0;
+
+	while(1) {
+		if(r < l)
+			break;
+		i = (r + l)>>1;
+		k = data[i];
+		if(k > search)
+			r = i - 1;
+		else if(k < search)
+			l = i + 1;
+		else {
+			success = 1;
+			break;
+		}
+	}
+
+	if(success)
+		printf("    search success!\n    i:  %d,search:  %d  =  %d\n",i,search,data[i]);
+	else
+		printf("    search failed!\n    search:%d\n",search);
+
+	return 0;
+}
