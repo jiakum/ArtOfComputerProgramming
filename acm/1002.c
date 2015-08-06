5c5a845527e6a72c383fd0ed7637015f46ac3ae1
Jiakun Ma <jkma@actions-semi.com>
2015-08-06 13:23:16 +0800

    add gpu code. add exercise 1002 code.

---
+++ acm/1002.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ISLETTER(x) (((x>='A')&&(x<='Z')) ? 1 : 0)
+#define ISDIGIT(x)  (((x>='0')&&(x<='9')) ? 1 : 0)
+#define ISRAIL(x)   ((x=='-') ? 1 : 0)
+static inline char letter2digit(char letter)
+{
+	char digit = 0;
+
+	if((letter >= 'A') && (letter <= 'P'))
+		digit = (letter - 'A')/3 + '2';
+	else if((letter >= 'R') && (letter <= 'Y'))
+		digit = (letter - 'Q')/3 + '7';
+
+	return digit;
+}
+
+#define LINE_SIZE (32)
+#define TELEPHONE_LEN (7)
+#define ALIGN_INT_ADDRESS(p) (((unsigned long)p+0x3)&(~0x3))
+#define FREQ_ADDRESS(p) ((int *)ALIGN_INT_ADDRESS((unsigned long)p+TELEPHONE_LEN+1))
+
+static inline void print_telephone(char *buf)
+{
+	int freq;
+	if(!buf)
+		return;
+	freq = *FREQ_ADDRESS(buf);
+	if(freq > 1)
+		printf("%c%c%c-%c%c%c%c %d\n",buf[0],buf[1],buf[2],buf[3],buf[4],buf[5],buf[6],freq);
+}
+
+int main()
+{
+	int total,i,j,len,ptr;
+	int *freq;
+	char **strings;
+
+	scanf("%d\n",&total);
+	if(!(strings = (char**)malloc(total*sizeof(char*)))) {
+		fprintf(stderr,"fatal error!can not malloc %d memory.\n",(int)(total*sizeof(char*)));
+		return -1;
+	}
+	for(i=0;i<total;i++) {
+		char *str = (char*)malloc(LINE_SIZE);
+		if(!str) {
+			fprintf(stderr,"fatal error!can not malloc %d memory.\n",LINE_SIZE);
+			return -1;
+		}
+		strings[i] = str;
+		if(!fgets(str,LINE_SIZE,stdin)) {
+			fprintf(stderr,"fgets error!i:%d\n",i);
+			return -1;
+		}
+
+		len = -1; ptr = 0;
+		while((++len < LINE_SIZE) && (ptr < TELEPHONE_LEN)){
+			if(ISRAIL(str[len]))
+				continue;
+			else if(ISDIGIT(str[len])) 
+				str[ptr++] = str[len];
+			else if(ISLETTER(str[len])) 
+				str[ptr++] = letter2digit(str[len]);
+		}
+		str[TELEPHONE_LEN] = '\0';
+		freq = FREQ_ADDRESS(str);
+		*freq = 1;
+	}
+
+	for(i=0;i<total;i++) {
+		int cmp;
+		char *temp;
+		for(j=i+i;j<total;j++) {
+			if(!strings[i])
+				break;
+			if(!strings[j])
+				continue;
+			cmp = strncmp(strings[i],strings[j],TELEPHONE_LEN);
+			if(cmp == 0) {
+				freq = FREQ_ADDRESS(strings[i]);
+				(*freq)++;
+				free(strings[j]);
+				strings[j] = NULL;
+			} else if(cmp > 0) {
+				temp = strings[i];
+				strings[i] = strings[j];
+				strings[j] = temp;
+			}
+		}
+	}
+
+	for(i=0;i<total;i++) 
+		print_telephone(strings[i]);
+	return 0;
+}
