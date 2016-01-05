ad734ae492ede976d1e52432a5b062da45b7d218
Jiakun Ma <jkma@actions-semi.com>
2016-01-05 19:07:57 +0800

    modify 1002.c

--- acm/1002.c
+++ acm/1002.c
@@ -22,14 +22,17 @@ static inline char letter2digit(char letter)
 #define ALIGN_INT_ADDRESS(p) (((unsigned long)p+0x3)&(~0x3))
 #define FREQ_ADDRESS(p) ((int *)ALIGN_INT_ADDRESS((unsigned long)p+TELEPHONE_LEN+1))
 
-static inline void print_telephone(char *buf)
+static inline int print_telephone(char *buf)
 {
 	int freq;
 	if(!buf)
-		return;
-	freq = *FREQ_ADDRESS(buf);
-	if(freq > 1)
+		return 0 ;
+	freq = *FREQ_ADDRESS(buf) + 1;
+	if(freq > 1) {
 		printf("%c%c%c-%c%c%c%c %d\n",buf[0],buf[1],buf[2],buf[3],buf[4],buf[5],buf[6],freq);
+		return 1;
+	} else
+		return 0;
 }
 
 int main()
@@ -66,13 +69,15 @@ int main()
 		}
 		str[TELEPHONE_LEN] = '\0';
 		freq = FREQ_ADDRESS(str);
-		*freq = 1;
+		*freq = 0;
 	}
 
 	for(i=0;i<total;i++) {
 		int cmp;
 		char *temp;
-		for(j=i+i;j<total;j++) {
+		for(j=0;j<total;j++) {
+			if(i == j)
+				continue;
 			if(!strings[i])
 				break;
 			if(!strings[j])
@@ -80,10 +85,14 @@ int main()
 			cmp = strncmp(strings[i],strings[j],TELEPHONE_LEN);
 			if(cmp == 0) {
 				freq = FREQ_ADDRESS(strings[i]);
-				(*freq)++;
+				*freq += *FREQ_ADDRESS(strings[j])+1;
 				free(strings[j]);
 				strings[j] = NULL;
-			} else if(cmp > 0) {
+			} else if((cmp > 0) && (i < j)) {
+				temp = strings[i];
+				strings[i] = strings[j];
+				strings[j] = temp;
+			} else if((cmp < 0) && (i > j)) {
 				temp = strings[i];
 				strings[i] = strings[j];
 				strings[j] = temp;
@@ -91,7 +100,10 @@ int main()
 		}
 	}
 
+	j = 0;
 	for(i=0;i<total;i++) 
-		print_telephone(strings[i]);
+		j += print_telephone(strings[i]);
+	if(j == 0)
+		printf("No duplicates.\n");
 	return 0;
 }
